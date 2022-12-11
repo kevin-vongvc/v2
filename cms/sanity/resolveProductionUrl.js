6bb8b0ff81578d77108d7909b3ee804cd98f557e
Chi Vong <chivongv@gmail.com>
2022-12-11 18:44:24 +0100

    fix node_env

--- cms/sanity/resolveProductionUrl.js
+++
@@ -1,9 +0,0 @@
-const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET;
-const projectUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://chivongv.vercel.app'
-    : 'http://localhost:3000';
-
-export default function resolveProductionUrl(document) {
-  return `${projectUrl}/api/preview?secret=${previewSecret}&type=${document._type}&slug=${document.slug.current}`;
-}
